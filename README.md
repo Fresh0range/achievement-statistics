490d3249c6105b3c274c70ee5433467f8b7b5e73
Fresh0range <1834888164@qq.com>
2020-09-14 17:42:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Steam Achievement Statistics
+===
+## Requirements
+* Download python 3.
+* Set environment variables for python.exe and pip.exe.
+* Install bs4 with pip.Open a cmd window and type `pip install bs4`.
+
+## How to use this tool
+>* Before you use this tool,please ensure that you have already complete all requirements.
+>In order to check it,you can open a cmd window and type `python` and `pip`.
+* Browse achievement page of any game,right click in the page and select `View page source code`.
+* Copy the source code and save as `a.html`.
+* Move `a.html` to the same directory as `achievement-statistics.py`.
+* Double click `achievement-statistics.py` and then you can see the result named `statistics.csv`.
+
+## Why do i do this
+Recently, I was playing a game called payday2, which required 1000 achievements to unlock the weapon skin. 
+I'm not used to using the achievement system that comes with the game, 
+so I sift out the achievements to get 1000 achievements faster.
---
+++ achievement-statistics.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+import csv
+import os
+
+file_path = 'a.html'
+result_path = 'statistics.csv'
+
+
+def main():
+    """
+    1.read html from file_path
+    2.parse html tags and generate achievement list
+    3.write achievement list to result path
+    :return:
+    """
+    html = read()
+    achievement_list = process(html)
+    write(achievement_list)
+
+
+def read():
+    html = ''
+    if os.path.exists(file_path):
+        file = open(file_path, encoding='utf8')
+        html = file.read()
+        file.close()
+    return html
+
+
+def write(achievement_list):
+    if os.path.exists(result_path):
+        os.remove(result_path)
+    with open(result_path, 'a', encoding='gb18030', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['name', 'detail', 'unlock/lock ', 'unlock_time', 'process'])
+        for item in achievement_list:
+            writer.writerow(item)
+        f.close()
+
+
+def process(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    soup.prettify()
+    achievements = soup.findAll(attrs={'class': 'achieveTxt'})
+    achievement_list = []
+    for achievement in achievements:
+        item = []
+        # achievement name
+        name = achievement.h3.get_text().strip()
+        item.append(name)
+        # achievement detail
+        detail = achievement.h5.get_text().strip()
+        item.append(detail)
+        # achievement unlock time
+        unlock_time_tag = achievement.find(attrs={'class': 'achieveUnlockTime'})
+        if unlock_time_tag is not None:
+            unlock_time = unlock_time_tag.get_text().replace('<br>', '').strip()
+            item.append('Y')
+            item.append(unlock_time)
+        else:
+            item.append('N')
+            item.append(' ')
+        # achievement process
+        process_tag = achievement.find(attrs={'class': 'progressText nextToBar ellipsis'})
+        if process_tag is not None:
+            process_value = process_tag.get_text().strip()
+            item.append(process_value)
+        else:
+            item.append(' ')
+        # add item to list
+        achievement_list.append(item)
+    return achievement_list
+
+
+if __name__ == '__main__':
+    main()
